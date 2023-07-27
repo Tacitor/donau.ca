3ca96c231b5968f26df37bba91ab82bce8f9f1c4
Tacitor <lukasjkrampitz@gmail.com>
2023-07-26 22:03:11 -0400

    update the script to be able to scale an image to the settings I like

--- tools/index.js
+++ tools/index.js
@@ -1,1 +1,13 @@
-console.log('hello world 👋');
+const sharp = require('sharp');
+
+sharp('lighthouse_sunset.webp')
+    .resize(1000, 750, {
+        kernel: sharp.kernel.cubic,
+    })
+    .webp({
+        smartSubsample: true,
+        preset: 'photo',
+        effort: 6,
+        quality: 80
+    })
+    .toFile('output/test.webp');
