6ace142c5b079f58105bfaef471e1334dd2f6a74
Tacitor <lukasjkrampitz@gmail.com>
2023-07-26 23:35:49 -0400

    setup a new java project for making the auto html gen and JS array and naming.

---
+++ tools/Java/Auto/src/auto/Auto.java
@@ -0,0 +1,23 @@
+/*
+ * Lukas Krampitz
+ * Jul 26, 2023
+ * 
+ */
+package auto;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class Auto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("does this work too??");
+    }
+
+}
