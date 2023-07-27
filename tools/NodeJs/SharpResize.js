315a14110fd25ce0f2620b06210081b966066308
Tacitor <lukasjkrampitz@gmail.com>
2023-07-27 19:43:40 -0400

    refactor the Js code to make more sense

--- tools/NodeJs/SharpResize.js
+++ tools/NodeJs/SharpResize.js
@@ -1,11 +1,11 @@
 const sharp = require('sharp');
 
 let dims = [[256, 192], [830, 623], [1150, 863], [1510, 1133], [1800, 1350], [2048, 1536]]
-var input = 'lighthouse_sunset'
+var src = "images/pi_cropped.webp"
 
 for (var dim = 0; dim < dims.length; dim++) {
 
-    sharp(input + '.webp')
+    sharp(src + '.webp')
         .resize(dims[dim][0], dims[dim][1], {
             kernel: sharp.kernel.cubic,
         })
@@ -15,7 +15,7 @@ for (var dim = 0; dim < dims.length; dim++) {
             effort: 6,
             quality: 80
         })
-        .toFile(input + '-' + dims[dim][0] + '.webp');
+        .toFile(src + '-' + dims[dim][0] + '.webp');
 
 
 }
