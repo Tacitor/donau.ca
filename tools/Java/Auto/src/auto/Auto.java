8981f56cdbd3ca88bb9e364919a9a975953064a2
Tacitor <lukasjkrampitz@gmail.com>
2023-07-27 19:32:43 -0400

    finish the main output of the java code.

--- tools/Java/Auto/src/auto/Auto.java
+++ tools/Java/Auto/src/auto/Auto.java
@@ -7,6 +7,8 @@
  */
 package auto;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Tacitor
@@ -17,6 +19,9 @@ public class Auto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Example for the main output
+        //let dims = [[256, 192], [830, 623], [1150, 863], [1510, 1133], [1800, 1350], [2048, 1536]]
+        
         String input = "256\u00D7192, 1030\u00D7773, 1440\u00D71080, 1750\u00D71313, 2016\u00D71512";
         StringBuilder tempInput;
 
@@ -32,6 +37,25 @@ public class Auto {
             tempInput.append(newInput[i]);
         }
 
+        //overwrite the old input
+        input = tempInput.toString();
+        
+        //split the input at the commas
+        newInput = input.split(", ", 0);
+        
+        //setup the structor for the data in the most accessible way
+        String[][] easyAccess = new String[newInput.length][2];
+        
+        //loop through the dims
+        for (int i = 0; i < newInput.length; i++) {
+            easyAccess[i] = newInput[i].split("x");
+        }
+        
+        //Set up the final main output string
+        tempInput = new StringBuilder();
+        tempInput.append("let dims = ");
+        tempInput.append(Arrays.deepToString(easyAccess));
+        
         System.out.println(tempInput.toString());
     }
 
