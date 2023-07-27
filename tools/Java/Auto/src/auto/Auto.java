c36b0d14464c0da4e64322c277aa1c4019f48cf3
Tacitor <lukasjkrampitz@gmail.com>
2023-07-27 00:01:27 -0400

    add some java code to remove non ascii chars from the srcset sizes coppied from the respimagelint

--- tools/Java/Auto/src/auto/Auto.java
+++ tools/Java/Auto/src/auto/Auto.java
@@ -1,7 +1,9 @@
 /*
- * Lukas Krampitz
+ * Lukas Krampitz\u00D7
  * Jul 26, 2023
- * 
+ * A small program that will take in the recomended dimensions from Ausi/RespImageLint 
+ * and produces a JS array and copies to to the clipboard. This can then be pasted 
+ * into a NodeJs script that generates the images.
  */
 package auto;
 
@@ -15,9 +17,22 @@ public class Auto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World");
-        System.out.println("does this work too??");
+        String input = "256\u00D7192, 1030\u00D7773, 1440\u00D71080, 1750\u00D71313, 2016\u00D71512";
+        StringBuilder tempInput;
+
+        // first pass is to convert the unicode char into an 'x'
+        String newInput[] = input.split("\u00D7", 0);
+
+        // rebuild with x's instead
+        tempInput = new StringBuilder();
+        for (int i = 0; i < newInput.length; i++) {
+            if (i != 0) {
+                tempInput.append('x');
+            }
+            tempInput.append(newInput[i]);
+        }
+
+        System.out.println(tempInput.toString());
     }
 
 }
