423c19ac974c99ef2003c31d14b49fc202146ad3
Tacitor <lukasjkrampitz@gmail.com>
2023-08-14 18:17:27 -0400

    add in a build folder

--- tools/Java/GitHubActionFindAndReplace/src/githubactionfindandreplace/GitHubActionFindAndReplace.java
+++ tools/Java/GitHubActionFindAndReplace/src/githubactionfindandreplace/GitHubActionFindAndReplace.java
@@ -17,6 +17,7 @@ public class GitHubActionFindAndReplace {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi");
+        System.out.println("Okay");
     }
     
 }
