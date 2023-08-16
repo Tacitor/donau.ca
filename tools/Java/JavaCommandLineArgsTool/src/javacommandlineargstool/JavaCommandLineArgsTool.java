9370edc6b69f83360d0134fe8298d2c9bcd75ebb
Tacitor <lukasjkrampitz@gmail.com>
2023-08-16 15:43:19 -0400

    add in a new java project to test command line args in prod

---
+++ tools/Java/JavaCommandLineArgsTool/src/javacommandlineargstool/JavaCommandLineArgsTool.java
@@ -0,0 +1,25 @@
+/*
+ * Lukas Krampitz
+ * Aug 16, 2023
+ * 
+ */
+package javacommandlineargstool;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class JavaCommandLineArgsTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Error: You must run the program from the command line and use include the 3 required command line arguments.");
+        } else {
+            
+        }
+    }
+    
+}
